bd094fcf1a7455e0c7fd34bb9907576f3a3c9531
Mustapha Yusuf <85568073+mustyfx0@users.noreply.github.com>
2025-05-24 21:13:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
-// server.js
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*" }
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// When user visits '/', serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-let players = {};
+// Socket.IO logic
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-io.on("connection", (socket) => {
-  console.log("New connection:", socket.id);
-
-  // Assign role
-  const role = Object.values(players).some(p => p.role === "snake") ? "ball" : "snake";
-  players[socket.id] = { role };
-  socket.emit("assignRole", role);
-  console.log(`Assigned ${role} to ${socket.id}`);
-
-  // Broadcast movements
-  socket.on("move", (data) => {
-    socket.broadcast.emit("playerMove", { id: socket.id, ...data });
+  socket.on('playerMove', (data) => {
+    socket.broadcast.emit('playerMove', data);
   });
 
-  socket.on("disconnect", () => {
-    console.log("Disconnected:", socket.id);
-    delete players[socket.id];
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
   });
 });
 
+// Start server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("Server listening on port", PORT);
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
